259be900228c15c1c5910df34ad555b497eca910
hamachi25 <107383950+hamachi25@users.noreply.github.com>
2024-10-01 21:20:01 +0900

    add: deployment workflow for GitHub Pages and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://astronaut.github.io",
+    base: "/my-repo",
     integrations: [
         starlight({
             title: "d-Record",
